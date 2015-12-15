b87110097ba24410c6efef072854307c9d22f2e8
Luke Iannini <lukexi@me.com>
2015-12-15 04:22:51 -0800

    getNow fix

--- src/Graphics/GL/Pal/Utility.hs
+++ src/Graphics/GL/Pal/Utility.hs
@@ -1,7 +1,6 @@
 module Graphics.GL.Pal.Utility where
 import Control.Monad.Trans
 import Control.Monad
-import Data.Time.Clock.POSIX
 import Data.Time
 
 putStrLnIO :: MonadIO m => String -> m ()
@@ -10,20 +9,9 @@ putStrLnIO = liftIO . putStrLn
 printIO :: (Show s, MonadIO m) => s -> m ()
 printIO = putStrLnIO . show
 
-
--- profile :: MonadIO m => String -> Int -> m b -> m b
--- profile name indent action = do
---   before <- liftIO getCPUTime -- DOESN'T WORK ON WINDOWS
---   x <- action
---   after <- liftIO getCPUTime
---   let diff = (fromIntegral (after - before)) / (10^(12::Integer))
---       tabs = replicate indent '\t'
---   when (diff > 0.000000) $ 
---     liftIO $ printf "%s%s Computation time: %0.5f sec\n" tabs name (diff :: Double)
---   return x
-
 profile :: MonadIO m => String -> Int -> m b -> m b
 profile name indent action = do
+  -- getCPUTime does not work on Windows >:() so we use getCurrentTime instead
   before <- liftIO getCurrentTime
   x <- action
   after <- liftIO getCurrentTime
@@ -35,4 +23,4 @@ profile name indent action = do
   return x
 
 getNow :: (Fractional a, MonadIO m) => m a
-getNow = realToFrac <$> liftIO getPOSIXTime
+getNow = realToFrac . utctDayTime <$> liftIO getCurrentTime
