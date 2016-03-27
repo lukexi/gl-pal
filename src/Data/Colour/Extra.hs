c8e84b741fefbee19b31ceb4dad9ffa77b2d03e0
Luke Iannini <lukexi@me.com>
2016-03-27 15:34:23 -0700

    Add hsl/hslaColor

--- src/Data/Colour/Extra.hs
+++ src/Data/Colour/Extra.hs
@@ -4,5 +4,8 @@ import Data.Colour.RGBSpace.HSL
 import Graphics.GL
 import Linear
 
-hslColor :: GLfloat -> GLfloat -> GLfloat -> GLfloat -> V4 GLfloat
-hslColor h s l a = (uncurryRGB V4 $ hsl (h * 360) s l) a
+hslaColor :: GLfloat -> GLfloat -> GLfloat -> GLfloat -> V4 GLfloat
+hslaColor h s l a = (uncurryRGB V4 $ hsl (h * 360) s l) a
+
+hslColor :: GLfloat -> GLfloat -> GLfloat -> V4 GLfloat
+hslColor h s l = hslaColor h s l 1 
